e81e40d2e100bf33c11afafd916ccae4e4140cdb
Daniel Davoli <davolli@hotmail.com>
2022-04-05 23:54:22 -0300

    added simple logger in SpaceVerifier

--- src/main/java/io/davolli/tinycompiler/lexicalanalyzer/verifier/SpaceVerifier.java
+++ src/main/java/io/davolli/tinycompiler/lexicalanalyzer/verifier/SpaceVerifier.java
@@ -2,12 +2,16 @@ package io.davolli.tinycompiler.lexicalanalyzer.verifier;
 
 import io.davolli.tinycompiler.lexicalanalyzer.model.Token;
 import io.davolli.tinycompiler.lexicalanalyzer.model.TokenType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 import static io.davolli.tinycompiler.lexicalanalyzer.verifier.helper.TokenListHelper.joinTokensIfLastIsEqualsOrAdd;
 
 public class SpaceVerifier extends Verifier {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(SpaceVerifier.class);
 
     @Override
     protected boolean checkValidation(char item, List<Token> tokenList) {
@@ -16,7 +20,8 @@ public class SpaceVerifier extends Verifier {
 
     @Override
     protected List<Token> checkExecution(char item, List<Token> tokenList) {
-        var actualToken = new Token().setTokenType(TokenType.SPACE).setValue(String.valueOf(item));
-        return joinTokensIfLastIsEqualsOrAdd(tokenList, actualToken);
+        var newToken = new Token().setTokenType(TokenType.SPACE).setValue(String.valueOf(item));
+        LOGGER.info("Type: {} | Value: {}", newToken.getTokenType(), newToken.getValue());
+        return joinTokensIfLastIsEqualsOrAdd(tokenList, newToken);
     }
 }
