9726a4aa68cc2fcbc39b81d0b33fe380ef9168a8
Daniel Davoli <davolli@hotmail.com>
2022-04-14 22:25:41 -0300

    rename token division sinal to division sign

--- src/main/java/io/davolli/tinycompiler/lexicalanalyzer/model/TokenType.java
+++ src/main/java/io/davolli/tinycompiler/lexicalanalyzer/model/TokenType.java
@@ -1,18 +1,15 @@
 package io.davolli.tinycompiler.lexicalanalyzer.model;
 
 public enum TokenType {
-    IDENTIFIER,
     SPACE,
     LEFT_PAREN,
     RIGHT_PAREN,
-    DIVISION_SINAL,
+    DIVISION_SIGN,
     TIMES_SIGN,
-    COMMENT,
     INLINE_COMMENT,
     NUMBER,
     PLUS_SIGN,
     MINUS_SIGN,
-    NEW_LINE,
     UNKNOWN,
     EOF;
 }
