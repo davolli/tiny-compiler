c7065acd0eecbd7ec8b31683d0db3b8e155106b4
Daniel Davoli <davolli@hotmail.com>
2022-07-12 20:04:49 -0300

    add log before analyzers

--- src/main/java/io/davolli/tinycompiler/TinyCompiler.java
+++ src/main/java/io/davolli/tinycompiler/TinyCompiler.java
@@ -3,15 +3,22 @@ package io.davolli.tinycompiler;
 import io.davolli.tinycompiler.lexicalanalyzer.LexicalAnalyzer;
 import io.davolli.tinycompiler.semanticanalyzer.SemanticAnalyzer;
 import io.davolli.tinycompiler.syntaxanalyzer.SyntaxAnalyzer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class TinyCompiler {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(TinyCompiler.class);
+
     public static void main(String[] args) {
         final String code = "  //as  (1 \n 12/((5-1)*  2) +2  ";
+        LOGGER.info("-----Lexical Analyzer---");
         var lexicalAnalyzerResultTokenList = LexicalAnalyzer.execute(code);
+        LOGGER.info("-----Syntax Analyzer---");
         var syntaxTreeResponse = SyntaxAnalyzer.getInstance().setTokenList(lexicalAnalyzerResultTokenList)
                 .execute();
+        LOGGER.info("-----Semantic Analyzer---");
         var semanticResponse = SemanticAnalyzer.getInstance().execute(syntaxTreeResponse);
 
     }
